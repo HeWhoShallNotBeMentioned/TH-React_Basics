7bdfba656de56e746bab2a05a45615d6b0e32ec4
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-10 22:43:22 -0800

    create player component

--- app.jsx
+++ app.jsx
@@ -10,23 +10,37 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
+function Player(props) {
+  return (
+  <div className="player">
+    <div className="player-name">
+      {props.name}
+    </div>
+    <div className="player-score">
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {props.score} </div>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+};
+
+
+
 function Application(props){
   return (
     <div className="scoreboard">
       <Header title = {props.title}/>
       <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Monty Lily
-          </div>
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 45 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+        <Player name="Monty Lily" score={29} />
+        <Player name="Melanie Hetrick" score={41} />
       </div>
     </div>
   );
