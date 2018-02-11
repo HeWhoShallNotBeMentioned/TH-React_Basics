b623319a764dcae75cecb4e910d2550ff1bfd821
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-10 19:51:38 -0800

    add proptypes and default props to Application

--- app.jsx
+++ app.jsx
@@ -22,4 +22,12 @@ function Application(props){
   );
 }
 
+Application.propTypes = {
+  title: React.PropTypes.string.isRequired,
+}
+
+Application.defaultProps = {
+  title: "Scoreboard",
+}
+
 ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
