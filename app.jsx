bbaede503278c5347c9b51350fd34a0ee2568ab0
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-11 18:35:54 -0800

    changed counter to component class so it could be given a state

--- app.jsx
+++ app.jsx
@@ -31,19 +31,22 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter (props) {
-  return (
-  <div className="counter">
-    <button className="counter-action decrement"> - </button>
-    <div className="counter-score"> {props.score} </div>
-    <button className="counter-action increment"> + </button>
-  </div>
-  );
-}
+const Counter = React.createClass({
+  propTypes: {
+    score: React.PropTypes.number.isRequired,
+  },
 
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired,
-};
+  render: function(){
+    return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {this.props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+    );
+  }
+});
+
 
 function Player(props) {
   return (
