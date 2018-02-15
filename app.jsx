bc733929f8652ad68f1838b1fdff9c4d07f81735
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-14 22:56:58 -0800

    create application class and delete application function

--- app.jsx
+++ app.jsx
@@ -49,7 +49,7 @@ function Player(props) {
       {props.name}
     </div>
     <div className="player-score">
-      <Counter initialScore={props.score}/>
+      <Counter score={props.score}/>
     </div>
   </div>
   );
@@ -60,28 +60,30 @@ Player.propTypes = {
   score: React.PropTypes.number.isRequired,
 };
 
+const Application = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    players: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired,
+  },
 
-function Application(props){
-  return (
-    <div className="scoreboard">
-      <Header title = {props.title}/>
-      <div className="players">
-        {props.players.map(function(player){
-          return <Player name={player.name} score={player.score} key={player.id}/>
-        })}
+  render: function(){
+    return (
+      <div className="scoreboard">
+        <Header title = {this.props.title}/>
+        <div className="players">
+          {this.props.players.map(function(player){
+            return <Player name={player.name} score={player.score} key={player.id}/>
+          })}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+})
 
-Application.propTypes = {
-  title: React.PropTypes.string,
-  players: React.PropTypes.arrayOf(React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired,
-    id: React.PropTypes.number.isRequired,
-  })).isRequired,
-}
 
 Application.defaultProps = {
   title: "Scoreboard",
