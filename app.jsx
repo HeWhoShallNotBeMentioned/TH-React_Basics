e866d30f4712febc66ce683aa79f192e26189132
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-14 22:33:53 -0800

    redoing counter to have data managed higher in the application.

--- app.jsx
+++ app.jsx
@@ -28,40 +28,19 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-const Counter = React.createClass({
-  propTypes: {
-    initialScore: React.PropTypes.number.isRequired,
-  },
-
-  getInitialState: function() {
-    return {
-      score: this.props.initialScore,
-    }
-  },
-
-  incrementScore: function(e) {
-    this.setState({
-      score: (this.state.score + 1),
-    });
-  },
-
-  decrementScore: function(e) {
-    this.setState({
-      score: (this.state.score - 1),
-    });
-  },
-
-  render: function(){
-    return (
+function Counter (props) {
+  return (
     <div className="counter">
-      <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-      <div className="counter-score"> {this.state.score} </div>
-      <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      <button className="counter-action decrement" > - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment" > + </button>
     </div>
-    );
-  }
-});
+  );
+}
 
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired
+}
 
 function Player(props) {
   return (
