4a5619ec1b646ac4683e63d7abc902490138df26
Chris Underwood <cunderwoodmn@gmail.com>
2018-02-17 00:17:09 -0800

    put in stopwatch ticking functionality

--- app.jsx
+++ app.jsx
@@ -22,19 +22,44 @@ const Stopwatch = React.createClass({
   getInitialState: function() {
     return {
       running: false,
+      elapsedTime: 0,
+      previousTime: 0,
     }
   },
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 100);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  onTick: function(){
+    if (this.state.running) {
+      let now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      });
+    }
+    console.log('onTick');
+  },
   onStart: function(){
-    this.setState({ running: true });
+    this.setState({
+    running: true,
+    previousTime: Date.now(),
+    });
   },
   onStop: function(){
     this.setState({ running: false });
   },
   onReset: function(){
-
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    });
   },
   render: function() {
-  var startStop;
+  let seconds = Math.floor(this.state.elapsedTime / 1000);
+  let startStop;
   if (this.state.running) {
     startStop = <button onClick={this.onStop}>Stop</button>
   } else {
@@ -43,7 +68,7 @@ const Stopwatch = React.createClass({
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
+        <div className="stopwatch-time">{seconds}</div>
         { startStop }
         <button onClick={this.onReset}>Reset</button>
       </div>
